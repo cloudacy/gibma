de4bda95ea262b98fd728035689c27680b279716
Patrick Kuen <patrick.kuen11@gmail.com>
2021-05-25 22:15:03 +0200

    test: add test for fetching images

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,5 +1,6 @@
 import {request} from '../src/main'
 import test from 'ava'
+import {FileExtension, fromBuffer} from 'file-type'
 
 test('runs a basic get request', async (t) => {
   const res = await request('https://www.cloudacy.com')
@@ -30,4 +31,16 @@ test('json parse', async (t) => {
   const res = await request('https://reqres.in/api/users?page=2')
 
   t.true(res.json() !== undefined)
+})
+
+test('downloads image correctly', async (t) => {
+  const user = (await request<{data: {avatar: string}}>('https://reqres.in/api/users/2')).json()
+  t.assert(user?.data.avatar !== undefined)
+
+  const res = await request(user!.data.avatar)
+  const buffer = res.data!
+  const fileType = await fromBuffer(buffer)
+
+  t.assert(fileType !== undefined)
+  t.is<FileExtension>(fileType!.ext, 'jpg')
 })
